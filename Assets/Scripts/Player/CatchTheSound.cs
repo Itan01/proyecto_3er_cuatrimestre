c259277df715cfe615087968b44cf3dcdebe082d
Itan01 <69739062+Itan01@users.noreply.github.com>
2025-04-22 15:25:08 -0300

    Mapa_De_Prueba_22_4

--- Assets/Scripts/Player/CatchTheSound.cs
+++ Assets/Scripts/Player/CatchTheSound.cs
@@ -6,19 +6,16 @@ public class CatchTheSound : MonoBehaviour
 {
     private AbsStandardSoundMov _scriptSound;
 
-     void Update()
+     void Start()
     {
-        if(Input.GetMouseButtonUp(1))
-        {
-            Destroy(gameObject);
-        }
+            Destroy(gameObject,0.25f);
     }
     void OnTriggerEnter(Collider sound)
     {
         if (sound.gameObject.CompareTag("Sound"))
         {
             _scriptSound = sound.GetComponent<AbsStandardSoundMov>();
-            _scriptSound.SetTarget(transform, 1000.0f);
+            _scriptSound.SetTarget(transform, 30.0f);
         }
     }
     void OnTriggerExit(Collider sound)
