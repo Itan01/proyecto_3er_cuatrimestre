78cf90b10159245750494f20ef7438e5001e06ad
emebei <maxibisso@gmail.com>
2025-06-20 22:59:59 -0300

    ParaBuild
    Build

--- Assets/Scripts/Objects/Objects/AbstractObjects.cs
+++ Assets/Scripts/Objects/Objects/AbstractObjects.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-
 public class AbstractObjects : MonoBehaviour
 {
     protected Animator _animator;
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    [SerializeField]private List<AbstractEnemy> _enemies;
+    [SerializeField] private List<Light> _light;
+    [SerializeField] private List<AbstractObjects> _objects;
+
+    private void Awake()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        if (Player.GetComponent<PlayerManager>())
+        {
+            GameManager.Instance.Room = this;
+            foreach (var item in _light)
+            {
+                item.gameObject.SetActive(true);
+            }
+            foreach (var item in _enemies)
+            {
+                item.SetActivate(true);
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider Player)
+    {
+        if (Player.GetComponent<PlayerManager>())
+        {
+            foreach (var item in _objects)
+            {
+                if (item.gameObject.activeSelf)
+                {
+                    _objects.Remove(item);
+                }
+            }
+            foreach (var item in _light)
+            {
+                item.gameObject.SetActive(false);
+            }
+            foreach (var item in _enemies)
+            {
+                item.SetActivate(false);
+            }
+        }
+    }
+    //<summary>
+    //    Agrega a la lista para tener el control a la hora de activarlo/Desactivarlo
+    //<summary>
+    public void AddToList(AbstractEnemy Enemies)
+    {
+        _enemies.Add(Enemies);
+    }
+    public void AddToList(Light Lights)
+    {
+        _light.Add(Lights);
+    }
+    public void AddToList(AbstractObjects Object)
+    {
+        _objects.Add(Object);
+    }
+
+    public void ResetRoom()
+    {
+        //foreach(var item in _objects)
+        //{
+        //    item.gameObject.SetActive(true);
+        //}
+        foreach(var item in _enemies)
+        {
+            item.Respawn();
+        }
+    }
+
+}
