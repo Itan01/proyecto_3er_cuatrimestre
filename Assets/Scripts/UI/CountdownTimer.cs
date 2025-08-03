1e512e29d042c5949f7ac496897f2db044a99467
emebei <maxibisso@gmail.com>
2025-08-03 02:57:31 -0300

    TimerWIP
    me falta hacer que los enemigos te persigan cuando llega a 0

---
+++ Assets/Scripts/UI/CountdownTimer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] private float _timeLeft = 2400f;
+    [SerializeField] private TextMeshProUGUI _timerText;
+    private bool isRunning = true;
+
+    void Update()
+    {
+        if (!isRunning) return;
+
+        if (_timeLeft > 0)
+        {
+            _timeLeft -= Time.deltaTime;
+            UpdateTimerUI(_timeLeft);
+            if (_timeLeft < 60f)
+                _timerText.color = Color.red;
+        }
+        else
+        {
+            _timeLeft = 0;
+            isRunning = false;
+            UpdateTimerUI(_timeLeft);
+        }
+    }
+
+    private void UpdateTimerUI(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        _timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+
+   
+}
