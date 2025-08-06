68c8f888f97561c6a1eac47be793c0e94ba23b92
Itan01 <69739062+Itan01@users.noreply.github.com>
2025-08-05 22:01:58 -0300

    Aim_5_8

--- Assets/Scripts/UI/MainManager.cs
+++ Assets/Scripts/UI/MainManager.cs
@@ -7,6 +7,11 @@ public class MainManager : MonoBehaviour
 {
     public Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void StartSceneChange()
     {
         animator.SetTrigger("Start");
