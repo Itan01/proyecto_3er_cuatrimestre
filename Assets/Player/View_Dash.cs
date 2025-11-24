7c2d6c39d8f23f106c62a33a68875ad95248e083
Itan01 <69739062+Itan01@users.noreply.github.com>
2025-11-24 20:10:10 -0300

    Sounds_24_11

--- Assets/Player/View_Dash.cs
+++ Assets/Player/View_Dash.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 public class View_Dash : Abstract_View
 {
+    private AudioClip _clip;
     public View_Dash()
     {
+        _clip = AudioStorage.Instance.PlayerSound(EAudios.PlayerDash);
     }
     public void Execute()
     {
+        AudioManager.Instance.PlaySFX(_clip,1.0f);
         EventPlayer.Trigger(EPlayer.dash,3.0f);
         _animator.SetTrigger("Dash");
     }
