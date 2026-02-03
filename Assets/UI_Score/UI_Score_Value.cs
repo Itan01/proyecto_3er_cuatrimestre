2b83901a2f841b2a290205c8203f552a6b73f6f0
emebei <maxibisso@gmail.com>
2026-02-03 13:39:01 -0300

    Score_Color_BUG
    No se porque en runtime el vertex color ignora el valor que le inputeas por codigo y se vuelve blanco, los valores rgb se cambian pero se ponen como si fuera blanco

--- Assets/UI_Score/UI_Score_Value.cs
+++ Assets/UI_Score/UI_Score_Value.cs
@@ -8,22 +8,23 @@ public class UI_Score_Value : MonoBehaviour, IObserverScore
     private TextMeshProUGUI _text;
     private float _score;
     private float _finalScore;
-    Action VitualUpdate;
+    Action VirtualUpdate;
     private void Start()
     {
         GetComponentInParent<IObservableScore>().AddObs(this);
         _text = GetComponent<TextMeshProUGUI>();
         _text.text = $"${_score:000}";
-        VitualUpdate = null;
+        _text.color = new Color(166, 159, 148);
+        VirtualUpdate = null;
     }
     public void Execute(float Value, float AddValue)
     {
         _finalScore= Value + AddValue;
-        VitualUpdate =UpdateScore;
+        VirtualUpdate =UpdateScore;
     }
     private void Update()
     {
-        VitualUpdate?.Invoke();
+        VirtualUpdate?.Invoke();
     }
 
     private void UpdateScore()
@@ -32,8 +33,12 @@ public class UI_Score_Value : MonoBehaviour, IObserverScore
         if (_score >=_finalScore)
         {
             _score = _finalScore;
-            VitualUpdate = null;
+            VirtualUpdate = null;
         }
         _text.text = $"${(int)_score:000}";
+        if(_score >= 50) 
+        {
+            _text.color = new Color(185, 134, 59);
+        }
     }
 }
