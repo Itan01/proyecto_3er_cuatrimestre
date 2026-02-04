3ae3e3f95c48076432064755a0559a4364bbe987
Itan01 <69739062+Itan01@users.noreply.github.com>
2026-02-03 22:14:22 -0300

    Colors_3_2

--- Assets/UI_Score/UI_Score_Value.cs
+++ Assets/UI_Score/UI_Score_Value.cs
@@ -8,13 +8,18 @@ public class UI_Score_Value : MonoBehaviour, IObserverScore
     private TextMeshProUGUI _text;
     private float _score;
     private float _finalScore;
+    private int _index = 0;
+    [SerializeField] private AudioClip _clipMilestone;
+    [SerializeField] private int _milestoneValue = 1000;
+    [SerializeField] private Color[] _nextColors;
     Action VirtualUpdate;
     private void Start()
     {
         GetComponentInParent<IObservableScore>().AddObs(this);
         _text = GetComponent<TextMeshProUGUI>();
         _text.text = $"${_score:000}";
-        _text.color = new Color(166, 159, 148);
+        _text.color = _nextColors[_index];
+        //_text.color = new Color(166, 159, 148);
         VirtualUpdate = null;
     }
     public void Execute(float Value, float AddValue)
@@ -36,9 +41,17 @@ public class UI_Score_Value : MonoBehaviour, IObserverScore
             VirtualUpdate = null;
         }
         _text.text = $"${(int)_score:000}";
-        if(_score >= 50) 
+        CheckMilestone();
+    }
+
+    private void CheckMilestone()
+    {
+        if (_score >= _index * _milestoneValue)
         {
-            _text.color = new Color(185, 134, 59);
+            _index++;
+            if (_clipMilestone != null) AudioManager.Instance.PlaySFX(_clipMilestone, 1.0f);
+            if (_index >= _nextColors.Length) return;
+            _text.color = _nextColors[_index];
         }
     }
 }
